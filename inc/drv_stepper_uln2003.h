c8972e4e50c73ccfb8639d465dc76b5a9dcc33cc
Danil Borchevkin <danil.borchevkin@gmail.com>
2019-07-02 03:28:36 +0200

    WIP

--- inc/drv_stepper_uln2003.h
+++ inc/drv_stepper_uln2003.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+#define STEPPER_MAX_STATES  (8)
+
 typedef enum _err_stepper_uln2003
 {
     OK,
@@ -11,8 +13,8 @@ typedef enum _err_stepper_uln2003
 
 typedef enum _stepper_mode
 {
-    one_phase,
-    two_phase,
+    one_phase_on,
+    two_phase_on,
     half_stepping
 } stepper_mode_t;
 
@@ -25,11 +27,18 @@ typedef enum _stepper_direction
 typedef struct _stepper_uln2003
 {
     stepper_mode_t mode;
-    stepper_direction_t direction;   
+    stepper_direction_t direction;
+    uint32_t states[STEPPER_MAX_STATES];
+    uint8_t current_state;
+    uint8_t states_count;
+    uint32_t state_duration;
+    // Pointers to expternal functions
+    void (*StateWrite)(uint32_t state);  
+    void (*DelayMs)(uint32_t ms);
 } stepper_uln2003_t;
 
 err_stepper_uln2003_t STEPPER_ULN2003_Init(stepper_uln2003_t *stepper);
-err_stepper_uln2003_t STEPPER_ULN2003_DoSteps(stepper_uln2003_t *stepper, uint32_t steps);
-
+void STEPPER_ULN2003_Tick(stepper_uln2003_t *stepper);
+void STEPPER_ULN2003_DoSteps(stepper_uln2003_t *stepper, uint32_t steps);
 
 #endif // !_DRV_STEPPER_ULN2003_H
--- src/drv_stepper_uln2003.c
+++ src/drv_stepper_uln2003.c
@@ -1,2 +1,100 @@
 #include "drv_stepper_uln2003.h"
 
+static void __PopulateStates(stepper_uln2003_t * stepper)
+{
+    stepper->states_count = 4;
+
+    // TODO clear states
+    switch(stepper->mode)
+    {
+        case one_phase_on:
+            stepper->states_count = 4;
+            stepper->states[0] = 0x01;
+            stepper->states[1] = 0x02;
+            stepper->states[2] = 0x04;
+            stepper->states[3] = 0x08;
+            break;
+        
+        case two_phase_on:
+            stepper->states_count = 4;
+            stepper->states[0] = 0x01 | 0x02;
+            stepper->states[1] = 0x02 | 0x04;
+            stepper->states[2] = 0x04 | 0x08;
+            stepper->states[3] = 0x08 | 0x01;
+            break;
+
+        case half_stepping:
+            stepper->states_count = 8;
+            stepper->states[0] = 0x01;
+            stepper->states[1] = 0x01 | 0x02;
+            stepper->states[2] = 0x02;
+            stepper->states[3] = 0x02 | 0x04;
+            stepper->states[4] = 0x04;
+            stepper->states[5] = 0x04 | 0x08;
+            stepper->states[6] = 0x08;
+            stepper->states[7] = 0x08 | 0x01;
+            break;
+
+        default:
+            break;
+    }
+}
+
+static void __RenewStateIndex(stepper_uln2003_t *stepper) 
+{
+    int8_t buffer;
+
+    if(stepper->direction == clockwise)
+    {
+        buffer = stepper->current_state + 1;
+    }
+    else 
+    {
+        buffer = stepper->current_state - 1;
+    }
+
+    if(buffer < 0) 
+    {
+        stepper->current_state = stepper->states_count - 1;
+        return;
+    }
+
+    if(buffer > (stepper->states_count - 1)) 
+    {
+        stepper->current_state = 0;
+        return;
+    }
+
+    stepper->current_state = buffer;
+}
+
+err_stepper_uln2003_t STEPPER_ULN2003_Init(stepper_uln2003_t *stepper)
+{
+    err_stepper_uln2003_t ret = OK;
+
+    __PopulateStates(stepper);
+    stepper->current_state = 0;
+
+    return ret
+}
+
+void STEPPER_ULN2003_Tick(stepper_uln2003_t *stepper)
+{
+    stepper->StateWrite(stepper->current_state);
+    __RenewStateIndex(stepper);
+}
+
+void STEPPER_ULN2003_DoSteps(stepper_uln2003_t *stepper, uint32_t steps)
+{
+    uint32_t i = 0;
+    uint8_t j = 0;
+
+    for(i = 0; i < steps; i++)
+    {
+        for(j = 0; j < stepper->states_count; j++)
+        {
+            STEPPER_ULN2003_Tick(stepper);
+            stepper->DelayMs(stepper->state_duration);
+        }
+    }
+}
